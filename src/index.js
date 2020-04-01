0ecfafc16ff8b7949aee86c134669ac925fc3b40
Klim Semenov <klimsemenov86@gmail.com>
2020-04-01 06:38:04 +0300

    asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
+import styles from './style.scss';
+// import './style.scss';
+import Img from './image.png';
 
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add(styles.hello);
   
+    const myImg = new Image();
+    myImg.src = Img;
+
+    // element.appendChild(myImg);
+
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,54 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader, // css в отдельный файл
+          {
+            loader: 'css-loader',
+            options: {
+              modules: { localIdentName: '[name]__[local]' }, // css modules
+              importLoaders: 1
+            }
+          },
+          'postcss-loader', // для postcss-preset-env (который включает в себя autoprefixer)
+          'sass-loader' // + минифицирует стили по умолчанию
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts',
+            }
+          }
+        ],
+      }
+    ],
   },
 };
